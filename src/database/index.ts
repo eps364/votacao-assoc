5d7ebd1b790f428f09aa89acbf30817d77ea6924
Emerson Pereira da Silva <pereiradasilva.emerson@gmail.com>
2020-05-18 06:55:22 -0300

    Correções em Arquivos auxiliares

---
+++ src/database/index.ts
@@ -0,0 +1,11 @@
+import { createConnection } from 'typeorm';
+
+createConnection();
+// const connection = createConnection({
+//   type: 'postgres',
+//   host: 'localhost',
+//   port: 5432,
+//   username: 'docker',
+//   password: 'docker',
+//   database: 'votacao',
+// });
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import app from './app';
+import './database';
 
 app.listen(3333, () => {
-  console.log('🚀 Server started on port 3333!');
+  console.log('☑ Server started on port 3333!');
 });
