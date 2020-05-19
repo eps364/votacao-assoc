06a7c3aea9c65664f516e1c807565ef986cbac84
Emerson Pereira da Silva <pereiradasilva.emerson@gmail.com>
2020-05-18 21:25:23 -0300

    Validar Votação

--- src/services/Votar.ts
+++ src/services/Votar.ts
@@ -1,7 +1,8 @@
 import { getCustomRepository } from 'typeorm';
 import Voto from '../models/VotoModel';
 import VotoRepository from '../repositories/VotoRepository';
-import sessaoRepository from '../repositories/sessaoRepository';
+import SessaoRepository from '../repositories/SessaoRepository';
+import UsuarioRepository from '../repositories/UsuarioRepository';
 
 interface Request {
   sessao_id: string;
@@ -18,8 +19,25 @@ class Votar {
     const votoRepository = getCustomRepository(VotoRepository);
 
     // Regra Validar Sessao
+    const sessaoRepository = getCustomRepository(SessaoRepository);
+    const sessao = await sessaoRepository.findOne({ id: sessao_id });
+    if (!sessao) throw new Error('Sessão não encontrada!');
 
     // Regra Validar Usuario
+    const usuarioRepository = getCustomRepository(UsuarioRepository);
+    const usuario = await usuarioRepository.findOne({ id: usuario_id });
+    if (!usuario) throw new Error('Usuario não encontrada!');
+
+    const jaVotou = await votoRepository.find({
+      where: [
+        {
+          sessao_id,
+          usuario_id,
+        },
+      ],
+    });
+
+    if (jaVotou.length > 0) throw new Error('Usuario já votou nesta sessao!');
 
     const votoConc = votoRepository.create({ sessao_id, voto, usuario_id });
 
